45110db1741c2d423ea78f23d7818de8ea706938
Nikhil <nikhilkamale2000@gmail.com>
2022-08-10 23:33:56 +0530

    assignment get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -34,5 +34,39 @@ router.get('/student-details/:name', function(req, res){
     
     res.send('Dummy response')
 })
+router.get('/movies', function (req, res) {
+    let movies = ['The Gray Guy', 'RRR', 'The Black Phone', 'Bullet Train', 'Thor:Love and Thunder']
+    res.send(movies)
+})
+router.get('/movies/:indexNumber', function (req, res) {
 
+    let movies = ['3 idiots', 'pk', 'Raone', 'housefull', '']
+    if (req.params.indexNumber < movies.length) {
+        res.send(movies[req.params.indexNumber])
+    }
+    else {
+        res.send("You have not entered Right indexnumber")
+
+    }
+
+})
+
+router.get('/films',function(req,res){
+
+let films=[ {'id': 1,'name': 'The Gray Guy'}, {'id': 2,'name': 'RRR'}, {'id': 3,'name': 'Bullet Train'}, {'id': 4,'name': 'Thor:Love and Thunder'}]
+res.send(films)
+
+})
+router.get('/films/:filmId',function(req,res){
+    
+    let films=[ {'id': 1,'name': 'The Gray Guy'}, {'id': 2,'name': 'RRR'}, {'id': 3,'name': 'Bullet Train'}, {'id': 4,'name': 'Thor:Love and Thunder'}]
+    let filmId = req.params.filmId
+    for(let i = 0; i < films.length; i++){
+        let movie = films[i]
+        if(movie.id == filmId) {
+            return res.send(movie)
+        }
+    }
+    res.send("The number you have given is not any movie")
+})
 module.exports = router;
