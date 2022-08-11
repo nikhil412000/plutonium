8bbabf8484ce93239d9de76c37f48c5aae673403
Omprakash <omdhanurkar444@gmail.com>
2022-08-11 22:59:24 +0530

    post-api-players

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,49 +1,66 @@
 const express = require('express');
+const { object } = require('underscore');
 const underscore = require('underscore')
 
 const router = express.Router();
 
-// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
-// Your route code will look like this
-router.get("/sol1", function (req, res) {
-    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    let arr = [1, 2, 3, 5, 6, 7]
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
+router.post('/players', function (req, res) {
 
-    
-    let sumOfNumber = (arr[arr.length-1]*(arr[arr.length-1]+1))/2
-    //let sum=0
-    // for (let i = 0; i < arr.length; i++) {.
-    
-    //     sum += arr[i];
-    // }
-    sumOfArr = arr.reduce((a,b)=> a+b)
-    let missingNumber = sumOfNumber - sumOfArr
-
-    res.send({ data: missingNumber });
-});
-
-
-
-// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
-// Your route code will look like this
-router.get("/sol2", function (req, res) {
- //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
-        
-
- let arr= [33, 34, 35, 37, 38]
- let length= arr.length
-
-
- sumOfArr = arr.reduce((a,b)=> a+b)
-
- let first= arr[0]
- let last= arr.pop()
- let consecutiveSum= (length + 1) * (first+ last ) / 2
- let missingNumber= consecutiveSum - sumOfArr
-
- res.send(  { data: missingNumber  }  );
-
+    //LOGIC WILL COME HERE
+    let addPlayer = req.body
+    let flag = 0;
+    for (let i = 0; i < players.length; i++) {
+        if (addPlayer.name == players[i].name) {
+            flag = 1
+            res.send("person already exists")
+        }
+    }
+    if (flag === 0) {
+        players.push(addPlayer)
+    }
+    res.send({ data: players, status: true })
 })
 
+
+// router.post('/test-post',function(req , res){
+//     let arr = [12, "functionup"]
+//     let ele = req.body.element
+//     arr.push(ele)
+
+//     res.send({ msg: arr, status : true})
+// })
+
+
+
+
 module.exports = router;
-// adding this comment for no reason
