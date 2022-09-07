47ec0c22dd346260f329334e84fe2a3ca91b7325
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-09-07 21:24:12 +0530

    porject-1

--- src/middleweres/auth.js
+++ src/middleweres/auth.js
@@ -4,7 +4,7 @@ const tokenverify = function (req, res, next) {
     try {
         let token = req.headers["x-api-key"]
         if (token) {
-            let decodedToken =  jwt.verify(token, "Project-1-blogging-groupe-50")
+            let decodedToken = jwt.verify(token, "Project-1-blogging-groupe-50")
             let auther = decodedToken.AutherId
             req.AutherId = auther
             next();
@@ -12,25 +12,25 @@ const tokenverify = function (req, res, next) {
             return res.status(400).send({ status: false, msg: "token must be present" });
         }
     } catch (error) {
-        res.status(500).send({ status: false, error:error.message });
+        res.status(500).send({ status: false, error: error.message });
     }
 
 };
 
-const auth = async function(req,res,next){
+const auth = async function (req, res, next) {
     try {
-             let tokenAutherId = req.AutherId
-            let blogid = req.params.blogId
-            let findauther = await blogModel.findById(blogid).select()
-            let Auther = findauther.authorId
-            if (tokenAutherId == Auther) {
-                req.AutherId = AutherId     //Set an attribute in request object 
-                next();
-            } else {
-                return res.status(403).send({ status: false, msg:"Sorry You are not authorised" })
-            }
+        let tokenAutherId = req.AutherId
+        let blogid = req.params.blogId
+        let findauther = await blogModel.findById(blogid).select({ authorId: 1, _id: 0 })
+        let Auther = findauther.authorId
+        if (tokenAutherId == Auther) {
+            req.AutherId = AutherId     //Set an attribute in request object 
+            next();
+        } else {
+            return res.status(403).send({ status: false, msg: "Sorry You are not authorised" })
+        }
     } catch (error) {
-        res.status(500).send({ status: false, error:error.message })
+        res.status(500).send({ status: false, error: error.message })
     }
 }
 
