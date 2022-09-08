e7920d467f9840a560cf1ad2a8e7848a67f62b55
Nikhil <nikhilkamale2000@gmail.com>
2022-09-08 11:00:23 +0530

    Project 1

--- src/modeles/blogModel.js
+++ src/modeles/blogModel.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
+    
     body: {
         type: String,
         required: true
